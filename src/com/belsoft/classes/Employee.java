365c86f1a9c8d3c426f09bd3541a3a274855d274
salomirc <ciprian.salomir@gmail.com>
2020-07-03 23:29:08 +0300

    Java class OOP 1/4

---
+++ src/com/belsoft/classes/Employee.java
@@ -0,0 +1,60 @@
+package com.belsoft.classes;
+
+public class Employee {
+    private int baseSalary;
+    private int hourlyRate;
+
+    public static int numberOfEmployees;
+
+
+    public Employee(int baseSalary, int hourlyRate){
+        setBaseSalary(baseSalary);
+        setHourlyRate(hourlyRate);
+        numberOfEmployees++;
+    }
+
+    //reusing the code in the first constructor
+    public Employee(int baseSalary){
+        this(baseSalary, 0);
+    }
+
+    //second constructor but without reusing the code in the first one
+//    public Employee(int baseSalary){
+//        setBaseSalary(baseSalary);
+//        setHourlyRate(0);
+//    }
+
+    public static void printNUmbersOfemployees() {
+        System.out.println(numberOfEmployees);
+    }
+
+    private int getBaseSalary() {
+        return baseSalary;
+    }
+
+    private void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Salary cannot be 0 or less.");
+        this.baseSalary = baseSalary;
+    }
+
+    private int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    private void setHourlyRate(int hourlyRate) {
+        if (hourlyRate < 0)
+            throw new IllegalArgumentException("Hourly rate cannot be 0 or less.");
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int calculateWage(int extraHours) {
+        return baseSalary + (hourlyRate * extraHours);
+    }
+
+    //method overloading
+    //unfortunately NO default value for method parameters in JAVA !!!
+    public int calculateWage() {
+        return baseSalary;
+    }
+}
---
+++ src/com/belsoft/classes/Main.java
@@ -0,0 +1,11 @@
+package com.belsoft.classes;
+
+public class Main {
+    public static void main(String[] args) {
+        var employee = new Employee(50_000);
+//        var employee = new Employee(50_000, 20);
+        Employee.printNUmbersOfemployees();
+        int wage = employee.calculateWage(15);
+        System.out.println(wage);
+    }
+}
