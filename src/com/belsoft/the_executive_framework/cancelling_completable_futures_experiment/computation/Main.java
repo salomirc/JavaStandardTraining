e167642d973d08c4f222959bdc74b5c7eaaa1d5f
salomirc <ciprian.salomir@gmail.com>
2020-08-12 17:07:13 +0300

    Removing the CompletableFuture cancelling experiment.

--- src/com/belsoft/the_executive_framework/cancelling_completable_futures_experiment/computation/Main.java
+++
@@ -1,7 +0,0 @@
-package com.belsoft.the_executive_framework.cancelling_completable_futures_experiment.computation;
-
-public class Main {
-    public static void main(String[] args) {
-        LongRunningComputation.show();
-    }
-}
--- src/com/belsoft/the_executive_framework/cancelling_completable_futures_experiment/sleeping/Main.java
+++
@@ -1,7 +0,0 @@
-package com.belsoft.the_executive_framework.cancelling_completable_futures_experiment.sleeping;
-
-public class Main {
-    public static void main(String[] args) {
-        ThreadIsSleeping.show();
-    }
-}
